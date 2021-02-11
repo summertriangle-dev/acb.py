487957dfea978e1d25ae5f09ca0502901515ea6e
kn <summertriangle.dev@gmail.com>
2021-02-10 16:44:45 -0800

    add packaging files and bump to v1.1.0

--- setup.py
+++ setup.py
@@ -4,18 +4,11 @@ from setuptools import setup, Extension
 
 def main():
     args = dict(
-        name="acb-py",
-        version="1.0.4",
-        description="Library for reading/extracting ACB files.",
-        packages=["acb"],
         ext_modules=[
             Extension(
                 "_acb_speedup", sources=["fast_sub/module.c"], py_limited_api=True
             )
         ],
-        entry_points = {
-            "console_scripts": ["acbextract=acb.__main__:main"],
-        },
     )
 
     setup(**args)
